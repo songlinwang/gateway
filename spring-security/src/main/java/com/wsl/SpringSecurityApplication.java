049c23c70201df16c62cfa4cbd02992b08717a71
wangsonglin <songlin_wangyo@163.com>
2019-06-17 20:20:24 +0800

    feat(spring security): limit maximum logining person

--- spring-security/src/main/java/com/wsl/SpringSecurityApplication.java
+++ spring-security/src/main/java/com/wsl/SpringSecurityApplication.java
@@ -2,12 +2,14 @@ package com.wsl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * @author wsl
  * @date 2019/5/22
  */
 @SpringBootApplication
+@EnableRedisHttpSession
 public class SpringSecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class, args);
